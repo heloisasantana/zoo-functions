8ebba44af0f35058b9b1d6f637e5cacff9089790
heloisasantana <heloisasantana.silva@hotmail.com>
2022-05-20 14:59:45 -0300

    [Heloísa Santana] Projeto Zoo Functions

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,9 +1,11 @@
 const data = require('../data/zoo_data');
 
+//Começando o projeto! Uhu!
+
 function countEntrants(entrants) {
   // seu código aqui
 }
-
+a
 function calculateEntry(entrants) {
   // seu código aqui
 }
