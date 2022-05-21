e80bc9b021f6e70dd2e01115c572b884a7ee2b46
heloisasantana <heloisasantana.silva@hotmail.com>
2022-05-20 22:56:22 -0300

    Corrigindo Erros Iniciais do Lint

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,9 @@
 const data = require('../data/zoo_data');
 
-//Começando o projeto! Uhu!
-
 function countEntrants(entrants) {
   // seu código aqui
 }
-a
+
 function calculateEntry(entrants) {
   // seu código aqui
 }
