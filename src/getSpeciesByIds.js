147bf61b5251e7e7588aff1a7319059fa6771bb0
heloisasantana <heloisasantana.silva@hotmail.com>
2022-05-22 12:30:19 -0300

    Conclusão do Requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,8 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+// Referência utilizada: https://youtu.be/bdLkAm12pgI;
+function getSpeciesByIds(...ids) {
+  return data.species.filter((specie) => ids.includes(specie.id));
 }
 
 module.exports = getSpeciesByIds;
