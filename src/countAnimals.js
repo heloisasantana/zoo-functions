9ce30649d26b891cd5bec15803dab0e774063eb5
heloisasantana <heloisasantana.silva@hotmail.com>
2022-05-24 18:52:56 -0300

    Conclusão do Requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,18 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (!animal) {
+    return data.species.reduce((accumulator, specie) => {
+      accumulator[specie.name] = specie.residents.length;
+      return accumulator;
+    }, {});
+  }
+  if (!animal.sex) {
+    const animalSpecie = data.species.filter(({ name }) => animal.specie === name);
+    return animalSpecie.reduce((accumulator, { residents }) => accumulator + residents.length, 0);
+  }
+  const animalSpecie = data.species.find(({ name }) => animal.specie === name);
+  return animalSpecie.residents.filter(({ sex }) => animal.sex === sex).length;
 }
 
 module.exports = countAnimals;
