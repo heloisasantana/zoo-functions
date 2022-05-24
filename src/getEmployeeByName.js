26745ba1fbecbf01f1fd1a98bea77a28802e68de
heloisasantana <heloisasantana.silva@hotmail.com>
2022-05-24 18:10:24 -0300

    Conclusão do Requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,13 @@
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (!employeeName) { return {}; }
+  if (data.employees.some((employee) => employee.firstName === employeeName)) {
+    return data.employees.find(({ firstName }) => (employeeName === firstName));
+  }
+  if (data.employees.some((employee) => employee.lastName === employeeName)) {
+    return data.employees.find(({ lastName }) => (employeeName === lastName));
+  }
 }
 
 module.exports = getEmployeeByName;
