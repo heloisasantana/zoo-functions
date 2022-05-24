585d487b42949844ffc0561c3c10f7069c86404d
heloisasantana <heloisasantana.silva@hotmail.com>
2022-05-24 19:17:32 -0300

    Conclusão do Requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,8 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const animalSpecie = data.species.filter(({ name }) => name === animal);
+  return animalSpecie[0].residents.every(({ age: animalAge }) => animalAge >= age);
 }
 
 module.exports = getAnimalsOlderThan;
