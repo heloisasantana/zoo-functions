16f5bf67a7690e0da02fcd7e88421f739889103a
heloisasantana <heloisasantana.silva@hotmail.com>
2022-05-24 21:11:55 -0300

    Conclusão do Requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,17 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  const managedList = data.employees.filter(({ managers }) => managers.includes(id));
+  if (managedList.length === 0) { return false; }
+  return true;
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  const managedList = data.employees.filter(({ managers }) => managers.includes(managerId));
+  if (managedList.length === 0) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  return managedList.map(({ firstName, lastName }) => `${firstName} ${lastName}`);
 }
 
 module.exports = { isManager, getRelatedEmployees };
