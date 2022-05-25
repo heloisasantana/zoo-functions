76303eac79fb4a24a896d0a57a547da85f980ea3
heloisasantana <heloisasantana.silva@hotmail.com>
2022-05-25 20:47:23 -0300

    Conclusão do Requisito 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,17 @@
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const searchEmployee = data.employees.find((employee) => employee.id === id);
+  const searchResponsibleFor = searchEmployee.responsibleFor[0];
+  const searchAnimal = data.species.find((specie) => specie.id === searchResponsibleFor);
+  const searchOldest = searchAnimal.residents.reduce((accumulator, resident) => {
+    if (resident.age > accumulator[2]) {
+      return [resident.name, resident.sex, resident.age];
+    }
+    return accumulator;
+  }, ['', '', 0]);
+  return searchOldest;
 }
 
 module.exports = getOldestFromFirstSpecies;
+// Referência para aplicação correta da HOF reduce com if e montagem do objeto final: monitoria com Rosalia Oliveira
