4f2969aeb0ad8d8a65821aa93ac4687007c2c3c5
heloisasantana <heloisasantana.silva@hotmail.com>
2022-05-26 00:27:29 -0300

    Conclusão do Requisito 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,22 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const countChild = entrants.filter((entrant) => entrant.age < 18).length;
+  const countAdult = entrants.filter((entrant) => entrant.age >= 18 && entrant.age < 50).length;
+  const countSenior = entrants.filter((entrant) => entrant.age >= 50).length;
+  return { child: countChild, adult: countAdult, senior: countSenior };
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!entrants) {
+    return 0;
+  }
+  if (Object.keys(entrants).length === 0) {
+    return 0;
+  }
+  const toPay = data.prices;
+  const people = Object.values(countEntrants(entrants));
+  return (toPay.child * people[0]) + (toPay.adult * people[1]) + (toPay.senior * people[2]);
 }
 
 module.exports = { calculateEntry, countEntrants };
